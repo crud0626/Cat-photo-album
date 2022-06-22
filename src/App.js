4fbd5847f8127d25c75cce67ccc818964eb6bf19
crud0626 <sisofiy6048@gmail.com>
2022-06-22 11:40:57 +0900

    Add request function in api.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Breadcrumb from "./components/BreadCrumb.js";
 import ImageView from "./components/ImageView.js";
 import Nodes from "./components/Nodes.js";
+import { request } from "./utils/api.js";
 
 export default class App {
     constructor($target) {
---
+++ src/utils/api.js
@@ -0,0 +1,14 @@
+const END_POINT = "https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev";
+
+export const request = async (nodeId) => {
+    const targetURL = nodeId ? `${END_POINT}/${nodeId}` : END_POINT;
+    try {
+        const res = await fetch(targetURL);
+
+        if (res.ok) {
+            return res.json();
+        }
+    } catch (e) {
+        throw new Error(`에러가 발생했습니다. ${e}`);
+    }
+}
